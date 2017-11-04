c1bdeff3f69f63ff67a4f975819bd92ca3123c19
WANG_QINGYUN <foodsready.market@gmail.com>
2017-11-04 11:59:08 +0800

    one line of comment

--- object.js
+++ object.js
@@ -106,6 +106,8 @@ printText(String(carBmw));
 /** to primitive conversion */
 
 /** constructor functions
+ main purpose of constructors – to implement reusable object creation code.
+
  They are named with capital letter first.
  They should be executed only with "new" operator.
 
