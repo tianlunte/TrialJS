0655315b5d8122d90e20c72a1ce893a160b658f5
WANG_QINGYUN <foodsready.market@gmail.com>
2017-11-06 10:35:10 +0800

    added a looping

--- arrays.js
+++ arrays.js
@@ -7,3 +7,18 @@
 /** two ways to declare an array */
 let arrayA = new Array();
 let arrayB = [];
+let arrayC = ["Apple", "Google", "Facebook", "Amazon", "Uber", "Airbnb"];
+
+
+
+/** looping */
+for (let company of arrayC) {
+    printText(company);
+}
+
+
+
+/** some common methods */
+function printText(text) {
+    process.stdout.write(text + "\n");
+}
