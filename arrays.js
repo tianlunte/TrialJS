d0090ee4e118ef071e5284a448bf44e9e9f08813
WANG_QINGYUN <foodsready.market@gmail.com>
2017-11-06 12:03:37 +0800

    added a demo for unshift

--- arrays.js
+++ arrays.js
@@ -20,7 +20,13 @@ for (let company of arrayC) {
 }
 
 
-/** array as a queue/stack */
+/** array as a queue/stack
+    push    --> add element to end of array
+    pop     --> extract last element of array and remove it from array
+    shift   --> extract first element and remove it from array
+    unshift --> add element to beging of array
+    important : push and unshift can add multiple elements at once
+ * */
 let queue = [];
 queue.push("Alibaba");
 queue.push("Tecent");
@@ -30,6 +36,8 @@ queue.push("Didi");
 printArray(queue);
 printText("pop 1 : " + queue.pop());
 printText("shift 1 : " + queue.shift());
+printArray(queue);
+queue.unshift("DBS", "OCBC", "UOB");
 printArray(queue);
 
 /** some common methods */
